296bce1255a25a180854051c94f3b55bc9a1b396
nwd3 <nwd3@students.calvin.edu>
2017-03-10 19:23:58 -0500

    added lab06_2

---
+++ 06sql/lab06_2.sql
@@ -0,0 +1,23 @@
+--Exercise 6.2
+--john from class and others helped on b and c
+--a.
+select avg(TRUNC((months_between(SYSDATE,p.birthdate)/12),-1)) "average age"
+	from person p;
+
+--b.--john and others helped on b and c
+select p.householdID, count(p.householdID) as t  --john helped me on this one and suggested I do it this way
+	from person p, household h
+		where p.householdID=h.id
+		and h.city='Grand Rapids'
+		group by householdID
+		having count(p.householdID)>=2
+		order by t desc;
+
+--c
+select p.householdID,h.phoneNumber, count(p.householdID) as t --john suggested I do it this way
+	from person p, household h
+		where p.householdID=h.id
+		and h.city='Grand Rapids'
+		group by p.householdID,h.phoneNumber
+		having count(p.householdID)>=2
+		order by t desc;
