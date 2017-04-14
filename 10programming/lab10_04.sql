1cef22a93bc22f667c34e4c1810cdd7cfd8c18c7
nwd3 <nwd3@students.calvin.edu>
2017-04-14 15:57:12 -0400

    added lab homework

---
+++ 10programming/lab10_04.sql
@@ -0,0 +1,27 @@
+Set transaction read write;
+CREATE OR REPLACE PROCEDURE incrementRank
+	(movieIdIn IN Movie.id%type, 
+	 deltaIn IN integer
+    ) AS
+	x Movie.rank%type;
+BEGIN
+
+	FOR i IN 1..50000 LOOP
+LOCK TABLE movie
+   IN EXCLUSIVE MODE;
+		SELECT rank INTO x FROM Movie WHERE id=movieIdIn;
+		UPDATE Movie SET rank=x+deltaIn WHERE id=movieIdIn;
+			dbms_output.put_line( 'This is x ' || x);
+		COMMIT;
+	END LOOP;
+
+END;
+/
+
+
+select rank from Movie where id =238071;
+ EXECUTE incrementRank(238071, 1);
+ 
+ --Explanation
+ --I was able to lock the table every time an update was change and both tables were allowed
+-- to update their data without any problem
