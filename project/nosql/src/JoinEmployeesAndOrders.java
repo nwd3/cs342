1cf7838b181118e177bfa9121586f68942e8ebe2
nwd3 <nwd3@students.calvin.edu>
2017-05-12 15:51:49 -0400

    addded doc

--- project/nosql/src/JoinEmployeesAndOrders.java
+++ project/nosql/src/JoinEmployeesAndOrders.java
@@ -6,7 +6,14 @@ import java.util.Map;
 import java.util.Scanner;
 
 /**
+*Evaluation of whether or not Oracles's KVLite key-value system is the best type of noSql database for my proj app:
+ Without looking into other nosql technologies I believe a keybased approach is a good way to implement the dndb database in sql.  I 
+ found it easy to map the current sql tables to the nosql key valued system quite intuitive.  Further for the things I might plan
+ on doing for the database like simple joins and sorting the tables orcales nosql system provides all I need to implement
+ a useful nosql database.
  * Created by nwd3 on 5/11/2017.
+ Why information they provide is appropriate for a noSQL solution:
+ This information uses the power of nosql's keys to join two tables together.  
  * This allows the user to input an order number and it joins the employee table to get the first and last names so that
  * You can see the order information Very useful if you forgot what employee number correspons to a given id and just to get
  * useful information about an order
